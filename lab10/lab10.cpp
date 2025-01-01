c6f527bc0a4494a5b1e30d5fcd1818e5eafbe0e2
Kris <sudoprogdoc@gmail.com>
2025-01-01 19:08:47 +0700

    Add project files.

---
+++ lab10/lab10.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void pack(int n, int *a, int *b)
+{
+    for (int i = 0;i < 18;i++) b[i] = 0;
+    for (int i = 0; i < n; i++) b[a[i]]++;
+}
+
+void unpack(int n, int *a, int *b)
+{
+    int k = 0;
+    for (int i = 0; i < 18; i++)
+        for (int l = 0; l < b[i];l++)
+        {
+            a[k] = i;
+            k++;
+        }
+}
+
+int main()
+{
+    const int n = 1500;
+    int a[n];
+    int b[18];
+    a[0] = 0;
+
+    for (int i = 1; i < n; i++) 
+        if (a[i - 1] < 17) a[i] = a[i - 1] + rand() % 2;
+            else a[i] = 17;
+
+    pack(n, a, b);
+    for (int i = 0;i < 18;i++) cout << "Чисел " << i << ": " << b[i] << endl;
+    unpack(n, a, b);
+}
